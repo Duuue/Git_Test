47debc9604249b884fd2f58d28dfffba8735b2e1
duyae <rd371@netvox.com.cn>
2023-03-30 14:36:34 +0800

    tag02

---
+++ src/main/java/com/example/gittest/TagTestContoller.java
@@ -0,0 +1,15 @@
+package com.example.gittest;
+
+/**
+ * @Package: com.example.gittest
+ * @ClassName: TagTestContoller
+ * @Author: DuYae
+ * @Description: //TODO
+ * @Date: 2023.3.30 14:34
+ * @Version: 1.0
+ */
+public class TagTestContoller {
+  public void setTag(){
+    System.out.println("This is a tag, tag02");
+  }
+}
