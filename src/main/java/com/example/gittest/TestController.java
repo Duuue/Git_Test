32d53134cda94d2c14792575ca15e03c8256f19f
duyae <rd371@netvox.com.cn>
2023-03-30 11:12:12 +0800

    add TestController

---
+++ src/main/java/com/example/gittest/TestController.java
@@ -0,0 +1,12 @@
+package com.example.gittest;
+
+/**
+ * @Package: com.example.gittest
+ * @ClassName: TestController
+ * @Author: CaiYanPing
+ * @Description: //TODO
+ * @Date: 2023.3.30 11:11
+ * @Version: 1.0
+ */
+public class TestController {
+}
