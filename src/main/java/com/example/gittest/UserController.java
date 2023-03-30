14ccec5550111c706090125d8e08b4e4fcab0b37
duyae <rd371@netvox.com.cn>
2023-03-30 14:09:43 +0800

    计算字符串次数

---
+++ src/main/java/com/example/gittest/UserController.java
@@ -0,0 +1,16 @@
+package com.example.gittest;
+
+/**
+ * @Package: com.example.gittest
+ * @ClassName: UserController
+ * @Author: DuYae
+ * @Description: //TODO
+ * @Date: 2023.3.30 14:07
+ * @Version: 1.0
+ */
+public class UserController {
+  public int getCount(String str1, String str2){
+    int count = 0;
+    return count;
+  }
+}
